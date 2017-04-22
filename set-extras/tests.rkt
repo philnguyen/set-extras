ea5cff1bfb6968cad2127145f32a045be498c425
Phil Nguyen <pcn@cs.umd.edu>
2017-04-22 00:47:07 -0400

    test

--- set-extras/tests.rkt
+++ set-extras/tests.rkt
@@ -20,3 +20,7 @@
 (define-values (evens odds) (set-partition even? (set 0 1 2 3 4 5 6 7 8 9)))
 (check-equal? evens (set 0 2 4 6 8))
 (check-equal? odds  (set 1 3 5 7 9))
+
+(check-equal? (for/union : (℘ Integer) ([n '(1 2 3 4)])
+                {set n (* 2 n)})
+              {set 1 2 3 4 2 4 6 8})
